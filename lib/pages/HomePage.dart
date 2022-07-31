9275cf0e31922741b8c9f7f6b620d0f71e4b710e
Mahmoud Elkafrawy@ELKAFRAWY <mahmoud.kafrawy96@gmail.com>
2022-07-31 17:51:21 +0200

    AppBar & Home

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,8 +1,60 @@
+import 'package:ecommerce/widgets/HomeAppBar.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: ListView(
+        children: [
+          HomeAppBar(),
+          Container(
+            height: 500,
+            padding: EdgeInsets.only(top: 15),
+            decoration: const BoxDecoration(
+              color: Color(0xFFedecf2),
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(25),
+                topRight: Radius.circular(25),
+              ),
+            ),
+            child: Column(
+              children: [
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 15),
+                  padding: EdgeInsets.symmetric(horizontal: 15),
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: Colors.white,
+                  ),
+                  child: Row(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(left: 5),
+                        height: 50,
+                        width: 300,
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                              border: InputBorder.none, hintText: "Search..."),
+                        ),
+                      ),
+                      Spacer(),
+                      const Icon(
+                        Icons.search,
+                        color: Color(
+                          0xFF4c53a5,
+                        ),
+                        size: 30,
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
