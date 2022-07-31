705dfa5f506fad8985b1fd1c53f4cdbbb20ade7c
Mahmoud Elkafrawy@ELKAFRAWY <mahmoud.kafrawy96@gmail.com>
2022-07-31 16:03:59 +0200

    pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,14 @@
-import 'package:ecommerce/pages/HomePage.dart';
+import 'package:ecommerce/pages/Cart.dart';
+import 'package:ecommerce/pages/Categories.dart';
+import 'package:ecommerce/pages/CheckOut.dart';
+import 'package:ecommerce/pages/Favorites.dart';
+import 'package:ecommerce/pages/Me.dart';
+import 'package:ecommerce/pages/NotSignedIn.dart';
+import 'package:ecommerce/pages/Payment.dart';
+import 'package:ecommerce/pages/Product.dart';
+import 'package:ecommerce/pages/SignedIn.dart';
 import 'package:flutter/material.dart';
+import 'package:ecommerce/pages/HomePage.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,7 +18,18 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(scaffoldBackgroundColor: Colors.white),
-      routes: {"/": (context) => HomePage()},
+      routes: {
+        "/": (context) => HomePage(),
+        "/categories": (context) => Categories(),
+        "/me": (context) => Me(),
+        "/favorites": (context) => Favorites(),
+        "/cart": (context) => Cart(),
+        "/product": (context) => Product(),
+        "/checkout": (context) => CheckOut(),
+        "/payment": (context) => Payment(),
+        "/signedin": (context) => SignedIn(),
+        "/notsignin": (context) => NotSignedIn(),
+      },
     );
   }
 }
