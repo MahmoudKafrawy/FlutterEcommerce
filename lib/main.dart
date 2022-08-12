710775fbc27cb94b7cd966d99f4651a748f1421d
Mostafa <mostafa.udemy1@gmail.com>
2022-08-13 01:18:42 +0200

    finished

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ import 'package:ecommerce/pages/Registration.dart';
 import 'package:ecommerce/pages/Search%20copy.dart';
 import 'package:ecommerce/pages/Search.dart';
 import 'package:ecommerce/pages/Settings.dart';
+import 'package:ecommerce/pages/auth_screen.dart';
 import 'package:ecommerce/providers/Brands_fetch.dart';
 import 'package:ecommerce/providers/Items_fetch.dart';
 import 'package:ecommerce/providers/favorites_counter.dart';
@@ -23,6 +24,8 @@ import 'package:provider/provider.dart';
 import 'package:ecommerce/providers/Cart_counter.dart';
 import 'package:ecommerce/providers/Items_fetch.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import './providers/auth.dart';
+import './pages/splach_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -40,6 +43,9 @@ void main() async {
   runApp(
     MultiProvider(
         providers: [
+          ChangeNotifierProvider.value(
+            value: Auth(),
+          ),
           ChangeNotifierProvider(
             create: (context) => Counter(),
           ),
@@ -67,42 +73,53 @@ class MyApp extends StatelessWidget {
   //
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        scaffoldBackgroundColor: Colors.white,
-        // appBarTheme: AppBarTheme(
-        //   iconTheme: IconThemeData(color: Colors.black),
-        //   color: Colors.deepPurpleAccent,
-        //   foregroundColor: Colors.black,
-        //   systemOverlayStyle: SystemUiOverlayStyle(
-        //     //<-- SEE HERE
-        //     // Status bar color
-        //     statusBarColor: Colors.red,
-        //     statusBarIconBrightness: Brightness.dark,
-        //     statusBarBrightness: Brightness.light,
-        //   ),
-        // ),
-      ),
-      routes: {
-        // "/": (context) => Productss(),
-        "/": (context) => showHome ? HomePage() : OnBoardingPage(),
-        "/homepage": (context) => HomePage(),
-        "/categories": (context) => Categories(),
-        "/me": (context) => Me(),
-        "/favorites": (context) => Favorites(),
-        "/cart": (context) => Cart(),
-        "/product": (context) => Product(),
-        // "/products": (context) => Productss(brandName: "",),
-        "/checkout": (context) => CheckOut(),
-        "/payment": (context) => Payment(),
-        "/setting": (context) => Settings(),
-        "/login": (context) => LogIn(),
-        "/registration": (context) => Registration(),
-        "/location": (context) => LocationPicker(),
-        "/search": (context) => Search(),
-        "/search2": (context) => Search2(),
-      },
-    );
+    return Consumer<Auth>(
+        builder: (ctx, auth, _) => MaterialApp(
+              debugShowCheckedModeBanner: false,
+              theme: ThemeData(
+                scaffoldBackgroundColor: Colors.white,
+                // appBarTheme: AppBarTheme(
+                //   iconTheme: IconThemeData(color: Colors.black),
+                //   color: Colors.deepPurpleAccent,
+                //   foregroundColor: Colors.black,
+                //   systemOverlayStyle: SystemUiOverlayStyle(
+                //     //<-- SEE HERE
+                //     // Status bar color
+                //     statusBarColor: Colors.red,
+                //     statusBarIconBrightness: Brightness.dark,
+                //     statusBarBrightness: Brightness.light,
+                //   ),
+                // ),
+              ),
+              home: auth.isAuth
+                  ? OnBoardingPage()
+                  : FutureBuilder(
+                      future: auth.tryAutoLogin(),
+                      builder: (ctx, authResultSnapshot) =>
+                          authResultSnapshot.connectionState ==
+                                  ConnectionState.waiting
+                              ? SplashScreen()
+                              : AuthScreen(),
+                    ),
+              routes: {
+                // "/": (context) => Productss(),
+
+                "/homepage": (context) => HomePage(),
+                "/categories": (context) => Categories(),
+                "/me": (context) => Me(),
+                "/favorites": (context) => Favorites(),
+                "/cart": (context) => Cart(),
+                "/product": (context) => Product(),
+                // "/products": (context) => Productss(brandName: "",),
+                "/checkout": (context) => CheckOut(),
+                "/payment": (context) => Payment(),
+                "/setting": (context) => Settings(),
+                "/login": (context) => LogIn(),
+                "/registration": (context) => Registration(),
+                "/location": (context) => LocationPicker(),
+                "/search": (context) => Search(),
+                "/search2": (context) => Search2(),
+              },
+            ));
   }
 }
